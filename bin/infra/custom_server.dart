f31969d4e0f7162dcdd3d94422c09fb6cc85bea1
Edvaldo <35712346+wsy152@users.noreply.github.com>
2022-07-17 20:31:51 -0300

    method .env

---
+++ bin/infra/custom_server.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+class CustomServer {
+  Future<void> initialize({
+    required Handler handler,
+    required String address,
+    required int port,
+  }) async {
+
+    await shelf_io.serve(handler, address, port);
+
+    log('Sevidor inicializado -> htt://$address:$port');
+  }
+}
